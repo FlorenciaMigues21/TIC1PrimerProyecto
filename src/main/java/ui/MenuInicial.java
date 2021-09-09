cce9cd42268b3f3ec574301d904d86d0cec103b1
Florencia Migues <florenciamigues21@gmail.com>
2021-09-09 18:35:26 -0300

    Commit de UI

--- src/main/java/ui/MenuInicial.java
+++ src/main/java/ui/MenuInicial.java
@@ -96,7 +96,7 @@ public class MenuInicial {
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setControllerFactory(Main.getContext()::getBean);
 
-        Parent root = fxmlLoader.load(signUpMenu.class.getResourceAsStream("LogIn.fxml"));
+        Parent root = fxmlLoader.load(signUpMenu.class.getResource("LogIn.fxml"));
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.show();
