d227dacaae546171de23c5b99461af877d09c7cc
AgustinCaamanoFlores <54273840+AgustinCaamanoFlores@users.noreply.github.com>
2021-10-25 20:12:59 -0300

    Agregado get de Comentary

--- src/main/java/proyecto/business/entities_managers/ComentaryManager.java
+++ src/main/java/proyecto/business/entities_managers/ComentaryManager.java
@@ -3,9 +3,13 @@ package proyecto.business.entities_managers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import proyecto.business.entities.Comentary;
+import proyecto.business.entities.Publication;
+import proyecto.business.entities.Tourist;
 import proyecto.business.exceptions.ComentaryCreationError;
 import proyecto.business.exceptions.DataBaseError;
 import proyecto.business.persistence.ComentaryDAO;
+
+import java.util.Collection;
 
 @Service
 public class ComentaryManager {
@@ -21,6 +25,14 @@ public class ComentaryManager {
         }catch(Exception e){
             throw new DataBaseError("Error al guardar el comentario",e.getMessage());
         }
+    }
+
+    public Collection<Comentary> getComentaryOfPublication(Publication publication){
+        return controller.findAllByPublication(publication);
+    }
+
+    public Collection<Comentary> getComentaryOfTourist(Tourist tourist){
+        return controller.findAllByTurista(tourist);
     }
 
 }
