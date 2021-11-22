7b3fcf97314e6dc97527b8ffecbcd2b95c5370a6
Juan Manuel <juanma.latorre.hermida@gmail.com>
2021-11-22 00:09:15 -0300

    ?

--- src/main/java/proyecto/business/entities_managers/Busqueda/StartUpEvent.java
+++ src/main/java/proyecto/business/entities_managers/Busqueda/StartUpEvent.java
@@ -5,7 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Component
 @Slf4j
 @RequiredArgsConstructor
